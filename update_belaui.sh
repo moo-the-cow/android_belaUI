7673c9c5f2bb0cca80e3d1dbeaf439bad582b73b
moo-the-cow <info@edumail.at>
2021-09-13 10:30:09 +0200

    updated version

--- update_belaui.sh
+++ update_belaui.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #USERNAME=`getent passwd 1000 | cut -d: -f1`
-USERNAME=`ls /home/`
+USERNAME=`id -un 1000`
 cp ./*.json /tmp/ && rm -rf ./.git ./.github ./* && git clone https://github.com/moo-the-cow/belaUI . && mv /tmp/*.json ./ && chown -R $USERNAME .
--- update_srtla.sh
+++ update_srtla.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #USERNAME=`getent passwd 1000 | cut -d: -f1`
-USERNAME=`ls /home/`
+USERNAME=`id -un 1000`
 cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME .
