73edfb08b339470ed77b7f22c749be9fdbe739b8
moo-the-cow <info@edumail.at>
2021-09-13 10:09:07 +0200

    fixed EOL issue

--- update_srtla.sh
+++ update_srtla.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cd ../srtla && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make
+#USERNAME=`getent passwd 1000 | cut -d: -f1`
+USERNAME=`ls /home/`
+cd ../srtla && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME .
