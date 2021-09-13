57dc4c7ddfd04319d784efe5b753f06122b93d86
moo <34907770+moo-the-cow@users.noreply.github.com>
2021-09-13 09:22:13 +0200

    Update update_belaui.sh

--- update_belaui.sh
+++ update_belaui.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cp ./*.json /tmp/ && rm -rf ./.git ./.github ./* && git clone https://github.com/moo-the-cow/belaUI . && mv /tmp/*.json ./
+USERNAME=`getent passwd 1000 | cut -d: -f1`
+cp ./*.json /tmp/ && rm -rf ./.git ./.github ./* && git clone https://github.com/moo-the-cow/belaUI . && mv /tmp/*.json ./ && chown -R $USERNAME .
