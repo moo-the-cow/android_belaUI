2d091d992867aa41ca25f9c24d4c4ac215a2dcc9
moo-the-cow <info@edumail.at>
2021-09-13 14:17:59 +0200

    implemented rollback

--- update_srtla.sh
+++ update_srtla.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #USERNAME=`getent passwd 1000 | cut -d: -f1`
 USERNAME=`id -un 1000`
-cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME .
+cp -R ../srtla ../srtla_lastversion && cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME .
