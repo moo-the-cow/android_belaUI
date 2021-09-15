6cc42b8929e69d301476279d3f3fc5fe4e6a16ac
moo-the-cow <info@edumail.at>
2021-09-15 04:46:29 +0200

    change owner for backup folder too

--- update_srtla.sh
+++ update_srtla.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #USERNAME=`getent passwd 1000 | cut -d: -f1`
 USERNAME=`id -un 1000`
-cp -R ../srtla ../srtla_lastversion && cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME .
+rm -rf ../srtla_lastversion &&  cp -R ../srtla ../srtla_lastversion && cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME . && chown -R $USERNAME ../srtla_lastversion
