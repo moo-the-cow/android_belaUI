d9d986630b4de767bd9f9432269b7c82322b65be
moo <34907770+moo-the-cow@users.noreply.github.com>
2021-09-17 04:50:44 +0200

    Update update_belacoder.sh

--- update_belacoder.sh
+++ update_belacoder.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ##USERNAME=`getent passwd 1000 | cut -d: -f1`
 USERNAME=`id -un 1000`
-cd .. && rm -rf ./belacoder_lastversion && cp -R ./belacoder ./belacoder_lastversion && rm -rf ./belacoder && git clone https://github.com/moo-the-cow/belacoder && cd belacoder && make && chown -R $USERNAME . && chown -R $USERNAME ../belacoder_lastversion
+cd /home/$USERNAME && rm -rf ./belacoder_lastversion && cp -R ./belacoder ./belacoder_lastversion && rm -rf ./belacoder && git clone https://github.com/moo-the-cow/belacoder && cd belacoder && make && chown -R $USERNAME . && chown -R $USERNAME ../belacoder_lastversion
