74575d6ec5e868b2f32ff07636e8010fb4ab2df4
moo <34907770+moo-the-cow@users.noreply.github.com>
2021-09-17 05:02:51 +0200

    Update update_srtla.sh

--- update_srtla.sh
+++ update_srtla.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #USERNAME=`getent passwd 1000 | cut -d: -f1`
 USERNAME=`id -un 1000`
-rm -rf ../srtla_lastversion &&  cp -R ../srtla ../srtla_lastversion && cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME . && chown -R $USERNAME ../srtla_lastversion
+cd /home/$USERNAME/srtla && rm -rf ../srtla_lastversion &&  cp -R ../srtla ../srtla_lastversion && cd .. && rm -rf ./srtla && git clone https://github.com/BELABOX/srtla && cd srtla && make && chown -R $USERNAME . && chown -R $USERNAME ../srtla_lastversion
